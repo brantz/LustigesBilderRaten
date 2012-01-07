9b4961a5f28df74e96ec87e82b18b331d17cb033
Raphael Tessmer <tessmer@ruffson.com>
2012-01-07 09:19:48 +0100

    Struktur-Kram, Storyboard

---
+++ Lustiges Bilerraten/MainMenuVC.h
@@ -0,0 +1,13 @@
+//
+//  MainMenuVC.h
+//  Lustiges Bilderraten
+//
+//  Created by Raphael Teßmer on 06.01.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainMenuVC : UIViewController
+
+@end
