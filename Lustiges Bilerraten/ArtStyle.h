55f7b9e835621c7695796e9ca226e54f02fba56b
bechtle <sarahbechtle@googlemail.com>
2012-01-18 10:21:36 +0100

    Datenbank funktioniert

--- Lustiges Bilerraten/ArtStyle.h
+++ Lustiges Bilerraten/ArtStyle.h
@@ -7,12 +7,15 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <sqlite3.h>
+#import "PEMAppDelegate.h"
 
 @interface ArtStyle : NSObject {
     NSString* styleName;
     NSString* moreInfoLink;
     NSString* infoText;
     NSString* shortText;
+    NSString* hints;
 }
 
 @property (nonatomic,strong) NSString* styleName;
@@ -21,7 +24,6 @@
 @property(nonatomic, strong) NSString* shortText;
 
 -(ArtStyle*) initArtStyleWithName: (NSString*) style;
--(void) findmoreInfoLink;
--(void) findInfoText;
--(void) findShortText;
+-(void) initFromDataBase:(NSString*) link andText: (NSString*) text  andShortText: (NSString*) shortT;
+-(void) readStyleFromDB;
 @end
