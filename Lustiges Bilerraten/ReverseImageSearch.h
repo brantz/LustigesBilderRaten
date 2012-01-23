80c80d0feb4181aea5c517051cd7aeada694793d
brantz <tom.rudolf@gmx.de>
2012-01-23 18:05:56 +0100

    added short revImgSearch process description in header

--- Lustiges Bilerraten/ReverseImageSearch.h
+++ Lustiges Bilerraten/ReverseImageSearch.h
@@ -5,6 +5,16 @@
 //  Created by Thomas Rudolf on 20/1/12.
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
+//	Sadly Google doesnt provide an API for it's otherwise really awesome
+//	Reverse Image Search. So the request has to be done directly to Google's
+//	Web Interface. Since Objective C doesnt provide any HTTP Post request 
+//	method, that allows suitable configuration to do this, the request is 
+//	first sent to a server that redirects that request to Google. The response
+//	then is parsed and analyzed using Selenium in a headless instance of 
+//	Mozilla's Firefox. This has to be done because the Google's response
+//	contains AJAX elements that can't be parsed in plain static HMTL. The 
+//	parsed output is then sent back to this App.
+
 
 #import <Foundation/Foundation.h>
 
