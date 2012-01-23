2421203106e25eeb4e9b4a81f8d09ae998988a95
brantz <tom.rudolf@gmx.de>
2012-01-23 18:03:05 +0100

    code cosmetics. commited to prevent conflicts

--- Lustiges Bilerraten/ImageChooserVC.m
+++ Lustiges Bilerraten/ImageChooserVC.m
@@ -34,17 +34,21 @@
 #pragma mark - View lifecycle
 
 
-- (void) showMenu{   
+- (void) showMenu
+{   
     UIActionSheet* gameMenu = [[UIActionSheet alloc] initWithTitle:@""
-delegate:self cancelButtonTitle:@"Abbrechen" destructiveButtonTitle:@"Spiel Beenden" otherButtonTitles:@"Hauptmenü", @"Meine Galerie", nil];
+								delegate:self cancelButtonTitle:@"Abbrechen" 
+								destructiveButtonTitle:@"Spiel Beenden"
+								otherButtonTitles:@"Hauptmenü", @"Meine Galerie", nil];
         
     [gameMenu showInView:self.view];
 }
 
 //TODO: case:2
-- (void) actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex{
-    
-    switch (buttonIndex) {
+- (void) actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    switch (buttonIndex)
+	{
         case 0:
             [self quitGame];
             break;
@@ -70,17 +74,18 @@ delegate:self cancelButtonTitle:@"Abbrechen" destructiveButtonTitle:@"Spiel Been
 
 - (void)viewDidLoad
 {
-
     [super viewDidLoad];
     self.navigationItem.hidesBackButton = YES;
     //Generate a Menu Button for the Navigation Bar
-    UIBarButtonItem* menuBarButton = [[UIBarButtonItem alloc] initWithTitle:@"Menü" style:UIBarButtonItemStylePlain target:self action:@selector(showMenu)];
+    UIBarButtonItem* menuBarButton = [[UIBarButtonItem alloc] initWithTitle:@"Menü" 
+			style:UIBarButtonItemStylePlain target:self action:@selector(showMenu)];
     //Generate a Next [Question] Button for the Navigation Bar
-    UIBarButtonItem* nextBarButton = [[UIBarButtonItem alloc] initWithTitle:@"Weiter" style:UIBarButtonItemStyleBordered target:self action:@selector(showQuestion)];
+    UIBarButtonItem* nextBarButton = [[UIBarButtonItem alloc] initWithTitle:@"Weiter"
+			style:UIBarButtonItemStyleBordered target:self action:@selector(showQuestion)];
     
     self.navigationItem.leftBarButtonItem = menuBarButton;
     self.navigationItem.rightBarButtonItem = nextBarButton;
-    }
+}
 
 - (void)viewDidUnload
 {
@@ -99,28 +104,29 @@ delegate:self cancelButtonTitle:@"Abbrechen" destructiveButtonTitle:@"Spiel Been
 }
 
 
-- (void) showQuestion{
+- (void) showQuestion
+{
     QuestionVC* nextQuestion = [self.storyboard instantiateViewControllerWithIdentifier:@"QuestionView"];
     nextQuestion.myGame = self.myGame;
     [self.navigationController pushViewController:nextQuestion animated:YES];
 }
 
-- (void) quitGame{
+- (void) quitGame
+{
     [self dismissModalViewControllerAnimated:YES];
     UIAlertView* alert = [[UIAlertView alloc] initWithTitle:@"Spiel Beenden"
-    message:@"Wollen Sie das Spiel wirklich beenden?" delegate:self cancelButtonTitle:@"Abbrechen" otherButtonTitles:@"Beenden", nil];
+    message:@"Wollen Sie das Spiel wirklich beenden?" delegate:self 
+			cancelButtonTitle:@"Abbrechen" otherButtonTitles:@"Beenden", nil];
     [alert show];
-    
 }
 
-- (void) alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex{
-    
-    if (buttonIndex == 1) {
-        
+- (void) alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    if (buttonIndex == 1)
+	{
         GameOverVC* gameOver = [self.storyboard instantiateViewControllerWithIdentifier:@"GameOverView"];
         gameOver.myGame = myGame;
-        [self.navigationController pushViewController:gameOver animated:YES];
-        
+        [self.navigationController pushViewController:gameOver animated:YES];  
     }
     
 }
@@ -131,43 +137,49 @@ delegate:self cancelButtonTitle:@"Abbrechen" destructiveButtonTitle:@"Spiel Been
  Create a ImagePickerController and set self as it's delegate.
  Sourcetype defines that a new photo is going to be taken rather than chosen from camera roll. Finally the image picker is being presented as a modal view.
 */
-- (IBAction) takePicture{
-        imagePicker = [[UIImagePickerController alloc] init];
-        [imagePicker setDelegate:self];
-    if( [UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]){
+- (IBAction) takePicture
+{
+	imagePicker = [[UIImagePickerController alloc] init];
+	[imagePicker setDelegate:self];
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera])
+	{
         imagePicker.sourceType = UIImagePickerControllerSourceTypeCamera;
     } else {
         imagePicker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
     }
-        [self presentViewController:self.imagePicker animated:YES completion:NULL];
-        
-    }
-    
-        
-
-
-- (void) imagePickerController:(UIImagePickerController *)picker didFinishPickingImage:(UIImage *)image editingInfo:(NSDictionary *)editingInfo{
-    [self updatePicturePreview:image];
-    [self.imagePicker dismissModalViewControllerAnimated:YES];
+	[self presentViewController:self.imagePicker animated:YES completion:NULL];  
 }
 
-- (void) updatePicturePreview: (UIImage*) image{
-	//show activity indicator to show users that somethings's happpening while doing reverse image search in background
+- (void) imagePickerController:(UIImagePickerController *)picker 
+		 didFinishPickingImage:(UIImage *)image editingInfo:(NSDictionary *)editingInfo
+{
+    //show activity indicator to show users that somethings's happpening while doing reverse image search in background
 	UIActivityIndicatorView  *av = [[UIActivityIndicatorView alloc]initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
-	//av.frame=CGRectMake(145, 160, 25, 25);
-	[av setCenter:imagePicker.view.center];
+	av.frame=CGRectMake(145, 160, 25, 25);
+	[av setCenter:picker.view.center];
 	av.tag  = 1;
-	[imagePicker.view addSubview:av];
+	[self.view addSubview:av];
 	[av startAnimating];
-    [previewImage setImage:image];
-    //Update Image within Game
-    
-    //myGame = [[Game alloc] initGameWithPic:image];
+
+	[self updatePicturePreview:image];
 	
 	//remove activity indicator when done
-    [myGame nextRound:0 andFoto:image];
-	UIActivityIndicatorView *tmpimg = (UIActivityIndicatorView *)[imagePicker.view viewWithTag:1];
+	UIActivityIndicatorView *tmpimg = (UIActivityIndicatorView *)[self.imagePicker.view viewWithTag:1];
 	[tmpimg removeFromSuperview];
+	
+	//dismiss imagePickerController
+    [picker dismissModalViewControllerAnimated:YES];
+}
+
+- (void) updatePicturePreview: (UIImage*) image
+{
+	[previewImage setImage:image];
+	
+    //Update Image within Game
+	//myGame = [[Game alloc] initGameWithPic:image];
+	
+    [myGame nextRound:0 andFoto:image];
+	
 
     //********DEBUGGING************
     
