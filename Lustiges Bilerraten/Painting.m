9a1a221be1e09acbc7f60a419e09c78c4f167101
brantz <tom.rudolf@gmx.de>
2012-01-25 23:35:14 +0100

    refined sql statement. still needs algorithm for more cases of more than 1 searchresults

--- Lustiges Bilerraten/Painting.m
+++ Lustiges Bilerraten/Painting.m
@@ -10,32 +10,35 @@
 #import "ReverseImageSearch.h"
 
 @implementation Painting
+
 @synthesize nameOfPainting,styleOfPainting,artist,picture,year,paintingIsInDB;
 
 
-- (Painting*) initPaintingWithFoto: (UIImage*) foto {
+- (Painting*) initPaintingWithFoto: (UIImage*) foto
+{
     self->picture = foto;
     [self findPaintingName: foto];
     return self;
 }
 
-- (Painting*) initPaintingWithName:(NSString*) name andPic: (UIImage*) pic {
+- (Painting*) initPaintingWithName:(NSString*) name andPic: (UIImage*) pic
+{
     self.nameOfPainting = name;
     self.picture = pic;
    [self readPaintingFromDB];
     return self;
 }
 
-//TODO Name des Bildes muss gesetzt werden
--(void) findPaintingName: (UIImage*) foto {	
+-(void) findPaintingName: (UIImage*) foto
+{	
 	ReverseImageSearch* searchEngine = [ReverseImageSearch alloc];
 	self.nameOfPainting = [searchEngine getInfoOnImage:foto];
-    NSLog(@"ReverseImageSearch%@",self.nameOfPainting);
     [self readPaintingFromDB];
     
 }
 
--(void) initFromDataBase:(NSString*) myArtist andStyle: (NSString*) myStyle andYear: (NSString*) myYear {
+-(void) initFromDataBase:(NSString*) myArtist andStyle: (NSString*) myStyle andYear: (NSString*) myYear
+{
     self.artist = myArtist;
     self.year = myYear;
     [self findStyleOfPainting:myStyle];
@@ -49,48 +52,57 @@
 }
 
 
-- (void) readPaintingFromDB{
-    
+- (void) readPaintingFromDB
+{
     PEMAppDelegate *appDelegate = (PEMAppDelegate *)[[UIApplication sharedApplication] delegate];
-   // NSString* paintingName = self.nameOfPainting;
     NSString* databasePath = appDelegate.databasePath;
     sqlite3 *database;
     paintingIsInDB=false;
     // Open the database from the users filessytem
-	if(sqlite3_open([databasePath UTF8String], &database) == SQLITE_OK ) {
+	if(sqlite3_open([databasePath UTF8String], &database) == SQLITE_OK && self.nameOfPainting.length!=0)
+	{
 		// Setup the SQL Statement and compile it for faster access
-        
-        NSString* sqlStatement = [NSString stringWithFormat:@"SELECT DISTINCT * FROM paintings WHERE id = (SELECT DISTINCT referenced_painting FROM query_results WHERE UPPER(name) LIKE UPPER('%%%@%%'))", self.nameOfPainting];
+		NSMutableString* sqlStatement = [[NSMutableString alloc] initWithString:@"SELECT DISTINCT * FROM paintings WHERE id = (SELECT DISTINCT referenced_painting FROM query_results WHERE UPPER(name) LIKE UPPER('%%"];
+		NSArray* imageNameArray = [self.nameOfPainting componentsSeparatedByString:@" "];
+		BOOL firstOne = YES;
+		for (NSMutableString *s in imageNameArray)
+		{
+			if (s.length > 4)
+			{
+				if (!firstOne) [sqlStatement appendString:@" OR UPPER(name) LIKE UPPER('%%"];
+				firstOne = NO;
+				[sqlStatement appendString:s];
+				[sqlStatement appendString:@"%%')"];
+			}
+		}
+		[sqlStatement appendString:@")"];
+		NSLog(@"SQL STATEMENT: %@", sqlStatement);
 		sqlite3_stmt *compiledStatement;
         
-        if(sqlite3_prepare_v2(database, [sqlStatement UTF8String], -1, &compiledStatement, NULL) == SQLITE_OK) {
-            while(sqlite3_step(compiledStatement) == SQLITE_ROW && self.nameOfPainting.length!=0) {
-				NSLog(@"Entering while block");
+        if(sqlite3_prepare_v2(database, [sqlStatement UTF8String], -1, &compiledStatement, NULL) == SQLITE_OK)
+		{
+            while(sqlite3_step(compiledStatement) == SQLITE_ROW)
+			{
 				// Read the data from the result row
                 paintingIsInDB=true;
 				NSString *pArtist = [NSString stringWithUTF8String:(char *)sqlite3_column_text(compiledStatement, 2)];
 				NSString *pArtStyle = [NSString stringWithUTF8String:(char *)sqlite3_column_text(compiledStatement, 3)];
 				NSString *pYear = [NSString stringWithUTF8String:(char *)sqlite3_column_text(compiledStatement, 4)];
-                NSLog(@"Found Artist: %@", pArtist);
-				NSLog(@"Found ArtStyle: %@", pArtStyle);
-				NSLog(@"Found Year: %@", pYear);
-				
-                [self initFromDataBase:pArtist andStyle:pArtStyle andYear:pYear];
+				[self initFromDataBase:pArtist andStyle:pArtStyle andYear:pYear];
             }
             
-            if(!paintingIsInDB) {
+            if(!paintingIsInDB)
+			{
                 NSLog(@"Match not found, try again");
             }
-            
-        } else {
+        }
+		else
+		{
             NSLog(@"%s", sqlite3_errmsg(database));
 		}
-        
         // Release the compiled statement from memory
 		sqlite3_finalize(compiledStatement);
-        
     }
-    
     sqlite3_close(database);
 }
 
